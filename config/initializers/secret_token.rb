6325dbd0694ec2b661e8d90d5c4ad083f37207e9
Henke <henrik.enegard@gmail.com>
2012-12-17 17:14:58 +0100

    tomt projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lolwiki::Application.config.secret_token = '8fa330ac74f71a260e4b5d787fcd6265ce00be87e3dcf93030143ee6c013029c4aa1b3c1be33b8e3d9ea394f559091889d8c9d34c77f09a65bd441121646398b'
